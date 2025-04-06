3ef4cc0ff49920131d330127af9d956a813bc013
geo-gkez <georgegezeris@gmail.com>
2025-04-06 14:03:10 +0300

    refactor: update FindAll method to retrieve customers from database

--- internal/repository/customer_repository.go
+++ internal/repository/customer_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"log"
 	"org/gg/banking/internal/models"
 )
 
@@ -21,28 +22,24 @@ func NewCustomerRepository(db *sql.DB) ICustomerRepository {
 	}
 }
 func (c *customerRepository) FindAll() ([]models.Customer, error) {
-	customers := []models.Customer{
-		{
-			ID:        1,
-			FirstName: "John",
-			LastName:  "Doe",
-			Email:     "john.doe@example.com",
-			Phone:     "555-1234",
-		},
-		{
-			ID:        2,
-			FirstName: "Jane",
-			LastName:  "Smith",
-			Email:     "jane.smith@example.com",
-			Phone:     "555-5678",
-		},
-		{
-			ID:        3,
-			FirstName: "George",
-			LastName:  "Gkezeris",
-			Email:     "gg@gmail.com",
-			Phone:     "699999",
-		},
+	rows, err := c.db.Query("SELECT id, first_name, last_name, email, phone FROM customers")
+	if err != nil {
+		return nil, err
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(rows)
+
+	var customers []models.Customer
+	for rows.Next() {
+		var customer models.Customer
+		if err := rows.Scan(&customer.ID, &customer.FirstName, &customer.LastName, &customer.Email, &customer.Phone); err != nil {
+			return nil, err
+		}
+		customers = append(customers, customer)
 	}
 	return customers, nil
 }
